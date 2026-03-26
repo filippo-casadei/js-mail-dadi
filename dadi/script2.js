4bf08e7643788d4f0b3a2aae9c41b0d091082074
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-26 20:59:32 +0100

    - copiata consegna. - creato array "dadi".

---
+++ dadi/script2.js
@@ -0,0 +1,7 @@
+//Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// step 1: creo un array con i numeri da 1 a 6.
+const dadi = [1, 2, 3, 4, 5, 6];
+
