5395653c120aa89a3a71ce78ce2c1dd67ea0a676
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-26 18:44:33 +0100

    inserita consegna esercizio.

---
+++ mail/script.js
@@ -0,0 +1,4 @@
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all utente la sua email,
+// Controlla che sia nella lista di chi può accedere,
+// Stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
