19d221fe98b6eafebf8f30171ff6ca768b07f52e
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-26 21:22:18 +0100

    aggiunto step 2, creata una variabile per ricevere un numero casuale da 0 a 5.

--- dadi/script2.js
+++ dadi/script2.js
@@ -5,3 +5,9 @@
 // step 1: creo un array con i numeri da 1 a 6.
 const dadi = [1, 2, 3, 4, 5, 6];
 
+// step 2: devo scegliere un elemento a caso dall'array.
+// genero un decimale che trasformo in intero fra 0 e 5.
+// Math.random genera un numero <= 0 ma <1 escluso, moltiplicato per 6 arriverò
+// massimo a qualcosa tipo 5,99999, con math.floor arrotondo per difetto e il risultato
+// corrispodnerà al valore dell'indice di un elemento dell'array
+const indiceCasuale = Math.floor (Math.random()  *6);
