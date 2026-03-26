8af7e6dd21bdc7e598b2fb5d9cbfcfe6543bde9a
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-26 21:35:54 +0100

    aggiunta logica step 5 prova creazione if/ else if/ else.

--- dadi/script2.js
+++ dadi/script2.js
@@ -10,15 +10,17 @@ const dadi = [1, 2, 3, 4, 5, 6];
 // Math.random genera un numero <= 0 ma <1 escluso, moltiplicato per 6 arriverò
 // massimo a qualcosa tipo 5,99999, con math.floor arrotondo per difetto e il risultato
 // corrispodnerà al valore dell'indice di un elemento dell'array
-const indiceCasuale = Math.floor (Math.random()  *6);
+const indiceCasualeGiocatore = Math.floor (Math.random()  *6);
 
 // step 3: creo 2 variabili una tiroGiocatore l'altra tiroPc 
-let tiroGiocatore = dadi[indiceCasuale];
+let tiroGiocatore = dadi[indiceCasualeGiocatore];
 
 // qui mi sono reso conto che servono due indici casuali altrimenti se uso lo stesso,
 // giocatore e pc avranno sempre lo stesso numero.
 
-//step 4: creo un indice casuale per il pc e la sua variabile tiro.
+// step 4: creo un indice casuale per il pc e la sua variabile tiro.
 const indiceCasualePc = Math.floor (Math.random() *6);
 let tiroPc = dadi[indiceCasualePc];
 
+// step 5: creo un if dove se è maggiore TiroGiocatore, giocatore vince, se è maggiore
+// tiroPc, Pc vince e se esce lo stesso numero ci sarà un pareggio.
